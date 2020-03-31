5d897466160aff9007e4a87b4dc02a627acca2b1
Magnus Manske <mm6@sanger.ac.uk>
2020-03-31 16:09:44 +0100

    misc

--- api.py
+++ api.py
@@ -12,34 +12,88 @@ def load_config_file(filename="config.json"):
 
 def connect_db(db,schema=''):
     if schema == '':
-        schema = str(config[db]['schema'])
+        schema = str(config['databases'][db]['schema'])
     return mysql.connector.connect(
-        host=str(config[db]['host']),
-        user=str(config[db]['user']),
+        host=str(config['databases'][db]['host']),
+        user=str(config['databases'][db]['user']),
         database=schema,
-        port=str(config[db]['port']),
-        passwd=str(config[db]['password'])
+        port=str(config['databases'][db]['port']),
+        passwd=str(config['databases'][db]['password'])
     )
 
 def get_current_timestamp():
 	now = datetime.now()
 	return datetime.strftime(now,'%Y%m%d_%H%M%S')
 
+def get_text_id(db,text):
+	query = ("SELECT `id` FROM `text` WHERE `text`=%s")
+	args = [ text ]
+	cursor = db.cursor()
+	cursor.execute(query, args)
+	for (id) in cursor:
+		return id
+
+	query = "INSERT IGNORE INTO `text` (`text`) VALUES (%s)"
+	cursor = db.cursor()
+	cursor.execute(query, args)
+	return cursor.lastrowid
+
+def get_container_id(db,image):
+	query = "SELECT `id` FROM `container` WHERE `image`=%s"
+	args = [ image ]
+	cursor = db.cursor(buffered=True)
+	cursor.execute(query, args)
+	for (id) in cursor:
+		return id
+
+	query = "INSERT IGNORE INTO `container` (`image`) VALUES (%s)"
+	args = [ image ]
+	cursor = db.cursor(buffered=True)
+	cursor.execute(query, args)
+	return cursor.lastrowid
+
+def get_executable_id(db,container_id,executable):
+	cursor = db.cursor(buffered=True)
+	if executable=="":
+		query = "SELECT `id` FROM `executable` WHERE `container_id`=%s AND `name` IS NULL"
+		cursor.execute(query, [container_id] )
+	else:
+		query = "SELECT `id` FROM `executable` WHERE `container_id`=%s AND `name`=%s"
+		args = [container_id,executable]
+		cursor.execute(query, args )
+	for (id) in cursor:
+		return id
+
+	cursor = db.cursor(buffered=True)
+	if executable=="":
+		query = "INSERT IGNORE INTO `executable` (`container_id`) VALUES (%s)"
+		cursor.execute(query, [container_id] )
+	else:
+		query = "INSERT IGNORE INTO `executable` (`container_id`,`name`) VALUES (%s,%s)"
+		cursor.execute(query, [container_id,executable] )
+	return cursor.lastrowid
+
 def save_to_database(json):
+	db = connect_db ( 'pathdb_rw' )
+	container_id = get_container_id ( db , json["image"] )
+	executable_id = get_executable_id ( db , container_id , json["executable"] )
+	path_id = get_text_id ( db , json["path"] )
+	parameters_id = get_text_id ( db , json["parameters"] )
+	
+	query = "INSERT IGNORE INTO `module_usage` (`user`,`timestamp`,`executable_id`,`path`,`parameters`) VALUES (%s,%s,%s,%s,%s)"
+	args = (json["user"],json["timestamp"],executable_id,path_id,parameters_id)
+	cursor = db.cursor()
+	cursor.execute(query, args)
+	cursor.close()
+
 	return
 
-def save_to_file(json):
+def save_to_logfile(json):
 	if not 'logfile' in config:
 		return
-
-	print (json)
-	#output = "{user},{timestamp},{image},{path},{executable},{parameters}".format(json)
 	output = f'{json["user"]},{json["timestamp"]},{json["image"]},{json["path"]},{json["executable"]},{json["parameters"]}\n'
-	print (output)
-
 	with open(config["logfile"], "a") as logfile:
 		logfile.write(output)
-
 	return
 
 
@@ -123,8 +177,12 @@ def log():
 			json["parameters"] = ""
 		else:
 			json["parameters"] = json["parameters"].strip()
+		json["executable"] = json["executable"].strip()
+		json["image"] = json["image"].strip()
+		json["user"] = json["user"].strip()
+		json["path"] = json["path"].strip()
 		save_to_database(json)
-		save_to_file(json)
+		save_to_logfile(json)
 		ret["json"] = json
 	else:
 		ret["status"] = "ERROR: Missing JSON keys"
